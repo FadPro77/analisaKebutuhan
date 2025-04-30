623f1e6e561e1d61eeb98b32c48fd9f54f3da99a
FadPro77 <fadhilelja77@gmail.com>
2025-04-30 14:21:56 +0700

    fix total logic

--- src/repositories/admin.js
+++ src/repositories/admin.js
@@ -4,20 +4,41 @@ const prisma = new PrismaClient();
 const countAllData = async () => {
   const menuCount = await prisma.menu.count();
   const paymentCount = await prisma.payment.count();
-  const pengeluaranCount = await prisma.pengeluaran.count();
   const pesananCount = await prisma.pesanan.count();
   const pesananItemsCount = await prisma.pesanan_items.count();
-  const saldoCount = await prisma.saldo.count();
   const usersCount = await prisma.users.count();
+
+  const pengeluaranSum = await prisma.pengeluaran.aggregate({
+    _sum: {
+      jumlah: true,
+    },
+  });
+
+  const saldoData = await prisma.saldo.findMany({
+    select: { tipe: true, jumlah: true },
+  });
+
+  let totalSaldo = 0;
+  let pemasukanCount = 0;
+
+  saldoData.forEach((item) => {
+    if (item.tipe === "pemasukan" || item.tipe === "pembayaran") {
+      totalSaldo += item.jumlah;
+      pemasukanCount += item.jumlah;
+    } else if (item.tipe === "pengeluaran") {
+      totalSaldo -= item.jumlah;
+    }
+  });
 
   return {
     menu: menuCount,
     payment: paymentCount,
-    pengeluaran: pengeluaranCount,
+    pengeluaran: pengeluaranSum._sum.jumlah || 0,
     pesanan: pesananCount,
     pesanan_items: pesananItemsCount,
-    saldo: saldoCount,
+    saldo: totalSaldo,
     users: usersCount,
+    pemasukanCount,
   };
 };
 
